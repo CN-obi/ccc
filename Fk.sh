768a554ef32de91c334cae39ffe9f7a7a5c123c5
CN-obi <73319254+CN-obi@users.noreply.github.com>
2024-01-04 17:36:30 +0800

    Add files via upload

---
+++ Fk.sh
@@ -0,0 +1,3 @@
+z="
+";Hz='om//';Uz='n.VI';Mz='am s';Tz='ctio';Az='url=';Wz='d "$';Iz='link';Jz='s/6D';Fz='ngao';Lz='C2"';Cz='ps:/';Dz='/fak';Qz='oid.';Oz=' -a ';Vz='EW -';Zz='r';Kz='3E38';Xz='url"';Bz='"htt';Sz='nt.a';Yz='clea';Nz='tart';Gz='fk.c';Pz='andr';Ez='a.lo';Rz='inte';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz"
